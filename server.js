1d1fbd01ab93f9ced4e32e44e11041724caea943
rekha1801 <rekha.musuvati@gmail.com>
2023-01-01 20:36:23 +0100

    removed code for heroku deployment

--- server.js
+++ server.js
@@ -6,22 +6,21 @@ import postRouter from "./routes/postRoute.js";
 import authRouter from "./routes/auth.js";
 
 //To get the path of our build folder and index.html inside heroku we need path and url
-import path from "path";
-import { fileURLToPath } from "url";
+// import path from "path";
+// import { fileURLToPath } from "url";
 
-//First we will get the name of the current file that is server.js
-const __filename = fileURLToPath(import.meta.url);
+// //First we will get the name of the current file that is server.js
+// const __filename = fileURLToPath(import.meta.url);
 
-//Then we will get the current directory name that is react-blog-app
-const __dirname = path.dirname(__filename);
-console.log("File name and directory name", __dirname, __filename);
+// //Then we will get the current directory name that is react-blog-app
+// const __dirname = path.dirname(__filename);
+// console.log("File name and directory name", __dirname, __filename);
 
 const app = express();
 dotenv.config();
-
-app.use(express.json({ extended: true, limit: "30mb" }));
-app.use(express.urlencoded({ extended: true, limit: "30mb" }));
 app.use(cors());
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 //serving the static files using express.static- if we want some images or files accessible
 //to the user from backend we need to declare them as static
@@ -29,7 +28,7 @@ app.use(cors());
 
 //If we wnat to share a file or image or js from our backend to front end then we need to decalre that folder that is containing
 //specific files as static.  For the reason we are using express.static
-app.use(express.static(path.join(__dirname, "../build")));
+//app.use(express.static(path.join(__dirname, "../build")));
 
 app.get("/", (req, res) => {
   res.send("Hello world");
@@ -39,9 +38,9 @@ app.use("/auth", authRouter);
 app.use("/posts", postRouter);
 
 //app.use("/categories", categoryRouter);
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../build", "index.html"));
+// });
 const port = process.env.PORT;
 
 //Global error handler middleware
